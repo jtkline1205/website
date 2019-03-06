ccbc71fb7f2e2eb385d5b0587fc1e7cad4f55c44
John Kline <jtkline1205@gmail.com>
2019-03-06 18:43:39 -0500

    adding react component

---
+++ john_header.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const e = React.createElement;
+
+class JohnHeader extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { };
+  }
+
+  render() {
+	    return React.createElement(
+		  "div",
+		  {className: "text-center"},
+		  React.createElement("h1", null, "John T Kline"),
+		  React.createElement("h4", null, "Software Engineer"),
+		  React.createElement("h6", null, "Java | Javascript | SQL | Scrum")
+		);
+	}
+}
+
+const domContainer = document.querySelector('#john-header');
+ReactDOM.render(e(JohnHeader), domContainer);
+
+
