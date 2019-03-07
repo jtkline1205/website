b29fc276e1b6a910b52f7b3600b2e9fc21796f08
John Kline <jtkline1205@gmail.com>
2019-03-06 23:09:15 -0500

    converting to react components

--- bookshelf.js
+++ bookshelf.js
@@ -1,8 +1,8 @@
-function Book(props) {
+function Book(img, description) {
 	return React.createElement("td", null, 
-		  			React.createElement("img", {src: props.imgUrl, width: "100px"}, null),
+		  			React.createElement("img", {src: img, width: "100px"}, null),
 		  			React.createElement("br", null, null),
-                	props.description
+                	description
   				);
 }
 
@@ -21,44 +21,38 @@ class Bookshelf extends React.Component {
 		  React.createElement("table", {className: "bookshelf-table"}, 
 	  		React.createElement("tbody", null, 
 			  	React.createElement("tr", null, 
-			  		Book({
-			  			imgUrl:"images/tdd.jpeg", 
-			  			description:"I've started this and I'm excited to learn more about how the 3 steps of TDD lead to great software."
-			  		}),
-			  		Book({
-			  			imgUrl:"images/continuous_delivery.jpeg", 
-			  			description:"Continuous Integration and Continuous Delivery have been done in different ways at the companies I've worked. I look forward to reading through this one to see if there are best practices that can be followed for CD, no matter which technologies or tools are being used."
-			  		}),
-	  				Book({
-	  					imgUrl:"images/essential_scrum.jpeg", 
-	  					description:"I started this one back when I was working as ScrumMaster for one of the teams I was on. It was helpful as a primer on the different Scrum ceremonies and the different roles there are on a Scrum team. I definitely want to finish this one so I can help identify issues with Scrum teams I'm a part of in the future and help correct them."
-	  				}),
-	  				Book({
-	  					imgUrl:"images/clean_code.jpeg", 
-	  					description:"I've really enjoyed working through this one; I haven't finished it but it has a lot of good reference points for how clean code should look in general. Developers have varying opinions about clean code, but I think it's cool how this book attempts to set a baseline."
-	  				}),
-	  				Book({
-	  					imgUrl:"images/design_patterns.jpeg", 
-	  					description:"This one has been fun to step through - I especially enjoy the quirky examples that accompany each pattern, and the different types of sections in the book (like Q&A's, interviews, diagrams) that help keep the content fresh and exciting. Maybe after I finish this I'll check out the Gang of Four book on Design Patterns."
-	  				}),
+			  		Book("images/tdd.jpeg","I've started this and I'm excited to learn more about how the 3 steps of TDD lead to great software."),
+			  		Book("images/continuous_delivery.jpeg","Continuous Integration and Continuous Delivery have been done in different ways at the companies I've worked. I look forward to reading through this one to see if there are best practices that can be followed for CD, no matter which technologies or tools are being used."),
+	  				Book(
+	  					"images/essential_scrum.jpeg", 
+	  					"I started this one back when I was working as ScrumMaster for one of the teams I was on. It was helpful as a primer on the different Scrum ceremonies and the different roles there are on a Scrum team. I definitely want to finish this one so I can help identify issues with Scrum teams I'm a part of in the future and help correct them."
+	  				),
+	  				Book(
+	  					"images/clean_code.jpeg", 
+	  					"I've really enjoyed working through this one; I haven't finished it but it has a lot of good reference points for how clean code should look in general. Developers have varying opinions about clean code, but I think it's cool how this book attempts to set a baseline."
+	  				),
+	  				Book(
+	  					"images/design_patterns.jpeg", 
+	  					"This one has been fun to step through - I especially enjoy the quirky examples that accompany each pattern, and the different types of sections in the book (like Q&A's, interviews, diagrams) that help keep the content fresh and exciting. Maybe after I finish this I'll check out the Gang of Four book on Design Patterns."
+	  				),
 				),
 				React.createElement("tr", null, 
-					Book({
-						imgUrl:"images/coders_at_work.jpeg",
-						description:"I haven't started this yet but it sounded like it would be a cool window into different types of software professions."
-					}),
-					Book({
-						imgUrl:"images/pro_git.jpeg",
-						description:"This book has been helpful as I've gotten started learning Git, having come from an SVN background. I've preferred to use Git on the command line rather than using a tool like SourceTree, so having a reference like this is helpful."
-					}),
-					Book({
-						imgUrl:"images/five_dysfunctions.jpeg",
-						description:"I thought this was an interesting take on the common issues affecting teams both large and small, executive and non-executive - even though the account was fictional, the constructs surrounding team dysfunctions were very real and I could apply them mentally to teams I have been on in the past."
-					}),
-					Book({
-						imgUrl:"images/clean_code.jpeg",
-						description:"I didn't have much of a formal financial education, so investing and money management has been a new concept for me. I've heard a lot about Benjamin Graham and his ideas about value investing, and how his ideas have stood the test of time. I intend to finish this one as soon as possible."
-					})
+					Book(
+						"images/coders_at_work.jpeg",
+						"I haven't started this yet but it sounded like it would be a cool window into different types of software professions."
+					),
+					Book(
+						"images/pro_git.jpeg",
+						"This book has been helpful as I've gotten started learning Git, having come from an SVN background. I've preferred to use Git on the command line rather than using a tool like SourceTree, so having a reference like this is helpful."
+					),
+					Book(
+						"images/five_dysfunctions.jpeg",
+						"I thought this was an interesting take on the common issues affecting teams both large and small, executive and non-executive - even though the account was fictional, the constructs surrounding team dysfunctions were very real and I could apply them mentally to teams I have been on in the past."
+					),
+					Book(
+						"images/clean_code.jpeg",
+						"I didn't have much of a formal financial education, so investing and money management has been a new concept for me. I've heard a lot about Benjamin Graham and his ideas about value investing, and how his ideas have stood the test of time. I intend to finish this one as soon as possible."
+					)
 				)
 			)
 		  )
--- newswire.js
+++ newswire.js
@@ -1,3 +1,12 @@
+function NewsLink(linkHref, linkText) {
+	return React.createElement("li", null, 
+		  		React.createElement("a", 
+		  			{href:linkHref, target:"_blank"}, 
+		  			linkText
+	  			) 
+  			);
+}
+
 class Newswire extends React.Component {
   constructor(props) {
     super(props);
@@ -16,95 +25,26 @@ class Newswire extends React.Component {
 	  		  	React.createElement("a", {href: "http://www.web.com", target:"_blank"}, "Web.com"),
   			),
 		  React.createElement("ul", null, 
-		  	React.createElement("li", null, 
-		  		React.createElement("a", 
-		  			{href:"http://news.wjct.org/post/jacksonville-based-webcom-names-sharon-rowlands-new-ceo", target:"_blank"}, 
-		  			"Jacksonville-based Web.com names Sharon Rowlands as new CEO"
-	  			) 
-  			),
-  			React.createElement("li", null, 
-		  		React.createElement("a", 
-		  			{href:"https://www.bizjournals.com/jacksonville/news/2018/10/10/web-com-shareholders-decide-on-2-24-billion-sale.html", target:"_blank"}, 
-		  			"Web.com shareholders decide on $2.24B sale"
-	  			) 
-  			),
-  			React.createElement("li", null, 
-		  		React.createElement("a", 
-		  			{href:"https://www.marketwatch.com/story/webcom-to-be-acquired-by-siris-capital-in-all-cash-deal-valued-at-about-2-billion-2018-06-21", target:"_blank"}, 
-		  			"Web.com to be acquired by Siris Capital in all cash deal valued at $2B"
-	  			) 
-  			),
-  			React.createElement("li", null, 
-		  		React.createElement("a", 
-		  			{href:"https://globenewswire.com/news-release/2017/11/01/1172601/0/en/Web-com-Announces-Acquisition-of-Acquisio-A-Leader-in-Online-Marketing-Artificial-Intelligence.html", target:"_blank"}, 
-		  			"Web.com announces acquisition of Acquisio"
-	  			) 
-  			),
-  			React.createElement("li", null, 
-		  		React.createElement("a", 
-		  			{href:"https://therealdeal.com/2017/03/03/yodle-sublets-56k-sf-at-330-west-34th-street-to-outcome-health/", target:"_blank"}, 
-		  			"Yodle sublets 56k SF at 330 W 34th St to Outcome Health"
-	  			) 
-  			),
-  			React.createElement("li", null, 
-		  		React.createElement("a", 
-		  			{href:"https://www.forbes.com/sites/knowledgewharton/2016/04/07/yodle-how-a-startup-went-from-birth-to-buyout/#251b4a597961", target:"_blank"}, 
-		  			"Yodle: how a startup went from birth to buyout"
-	  			) 
-  			),
-  			React.createElement("li", null, 
-		  		React.createElement("a", 
-		  			{href:"https://www.bizjournals.com/newyork/news/2016/02/11/yodle-agrees-to-342takeover-offer-from-web-com.html", target:"_blank"}, 
-		  			"Yodle agrees to $342M takeover from Web.com"
-	  			) 
-  			),
-  			React.createElement("li", null, 
-		  		React.createElement("a", 
-		  			{href:"https://businessfacilities.com/2015/04/yodle-expands-in-new-york-city/", target:"_blank"}, 
-		  			"Yodle expands in NYC"
-	  			) 
-  			),
-  			React.createElement("li", null, 
-		  		React.createElement("a", 
-		  			{href:"https://therealdeal.com/2014/07/28/ad-platform-yodle-sets-up-hq-at-vornados-330-west-34th/", target:"_blank"}, 
-		  			"Yodle sets up HQ at Vornado's 330 W 34th"
-	  			) 
-  			),
-  			React.createElement("li", null, 
-		  		React.createElement("a", 
-		  			{href:"https://www.bizjournals.com/newyork/news/2013/03/04/yodle-acquires-lighthouse-practice.html", target:"_blank"}, 
-		  			"Yodle acquires Lighthouse practice management group"
-	  			) 
-  			)
-
-
+  			NewsLink("http://news.wjct.org/post/jacksonville-based-webcom-names-sharon-rowlands-new-ceo", "Jacksonville-based Web.com names Sharon Rowlands as new CEO"),
+  			NewsLink("https://www.bizjournals.com/jacksonville/news/2018/10/10/web-com-shareholders-decide-on-2-24-billion-sale.html", "Web.com shareholders decide on $2.24B sale"),
+  			NewsLink("https://www.marketwatch.com/story/webcom-to-be-acquired-by-siris-capital-in-all-cash-deal-valued-at-about-2-billion-2018-06-21", "Web.com to be acquired by Siris Capital in all cash deal valued at $2B"),
+  			NewsLink("https://globenewswire.com/news-release/2017/11/01/1172601/0/en/Web-com-Announces-Acquisition-of-Acquisio-A-Leader-in-Online-Marketing-Artificial-Intelligence.html", "Web.com announces acquisition of Acquisio"),
+  			NewsLink("https://therealdeal.com/2017/03/03/yodle-sublets-56k-sf-at-330-west-34th-street-to-outcome-health/", "Yodle sublets 56k SF at 330 W 34th St to Outcome Health"),
+  			NewsLink("https://www.forbes.com/sites/knowledgewharton/2016/04/07/yodle-how-a-startup-went-from-birth-to-buyout/#251b4a597961", "Yodle: how a startup went from birth to buyout"),
+  			NewsLink("https://www.bizjournals.com/newyork/news/2016/02/11/yodle-agrees-to-342takeover-offer-from-web-com.html", "Yodle agrees to $342M takeover from Web.com"),
+  			NewsLink("https://businessfacilities.com/2015/04/yodle-expands-in-new-york-city/", "Yodle expands in NYC"),
+  			NewsLink("https://therealdeal.com/2014/07/28/ad-platform-yodle-sets-up-hq-at-vornados-330-west-34th/", "Yodle sets up HQ at Vornado's 330 W 34th"),
+  			NewsLink("https://www.bizjournals.com/newyork/news/2013/03/04/yodle-acquires-lighthouse-practice.html", "Yodle acquires Lighthouse practice management group"),
 			),
 		  React.createElement("hr", null, null),
 		  React.createElement("h6", null, 
 		  	React.createElement("a", {href:"https://www.sapientgovernmentservices.com/us/about-us.html", target:"_blank"}, "Sapient Government Services")
 		  ),
 		  React.createElement("ul", null, 
-		  		React.createElement("li", null, 
-			  		React.createElement("a", 
-			  			{href:"https://www.adweek.com/agencies/publicis-groupe-merges-global-digital-networks-sapient-and-razorfish-174699/", target:"_blank"}, 
-			  			"Publicis merges Sapient and Razorfish"
-		  			) 
-	  			),
-	  			React.createElement("li", null, 
-			  		React.createElement("a", 
-			  			{href:"https://adage.com/article/agency-news/publicis-acquire-digital-giant-sapient-3-7-billion/295689/", target:"_blank"}, 
-			  			"Publicis acquires digital giant Sapient for $3.7 billion"
-		  			) 
-	  			),
-	  			React.createElement("li", null, 
-			  		React.createElement("a", 
-			  			{href:"https://www.businesswire.com/news/home/20140210006454/en/Sapient-Expands-Government-Services-Footprint-Acquisition-OnPoint", target:"_blank"}, 
-			  			"Sapient expands Government Services with acquisition of OnPoint"
-		  			) 
-	  			)
+		  	NewsLink("https://www.adweek.com/agencies/publicis-groupe-merges-global-digital-networks-sapient-and-razorfish-174699/", "Publicis merges Sapient and Razorfish"),
+		  	NewsLink("https://adage.com/article/agency-news/publicis-acquire-digital-giant-sapient-3-7-billion/295689/", "Publicis acquires digital giant Sapient for $3.7 billion"),
+		  	NewsLink("https://www.businesswire.com/news/home/20140210006454/en/Sapient-Expands-Government-Services-Footprint-Acquisition-OnPoint", "Sapient expands Government Services with acquisition of OnPoint")
 		  	)
-			
-		  
 		  );
 	}
 }
